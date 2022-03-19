aacc62e2e3cbea7db377bd59975bda4a811a53ec
ajurado301 <ajurado301@gmail.com>
2022-03-19 20:18:28 +0100

    enpoint login provisional

--- controllers/login.js
+++ controllers/login.js
@@ -4,25 +4,24 @@ const { dungeonsDB } = require('../bbdd');
 // Controlador endpoint /login
 // POST
 const postLogin = (req, res) => {
-    return res.status(200).send({ ok: true, message: `postLogin works!!` });
 
-    // const { correo, password } = req.body;
-    // let params = [correo, password];
-    // let sql = 'SELECT * FROM usuario WHERE (correo = ?) && (password = ?)';
-    // appbooksBBDD.query(sql, params, (error, result) => {
-    //     if (!error) {
-    //         let respuesta = { ok: false, message: `Usuario o contraseña incorrectos` };                
-    //         if (result.length > 0) {
-    //             const { id_usuario, nombre, apellidos, correo, url } = result[0]
-    //             let usuario = {  id_usuario, nombre, apellidos, correo, url };
-    //             respuesta = { ok: true, message: `Login correcto`, resultado: usuario };
-    //         }
-    //         return res.status(200).json(respuesta);
-    //     } else {
-    //         let respuesta = { ok: false, message: error.sqlMessage };
-    //         return res.status(200).json(respuesta);
-    //     }
-    // })
+    const { nameEmail, password } = req.body;
+    let params = [nameEmail, nameEmail, password];
+    let sql = 'SELECT * FROM User WHERE (name = ? || email = ?) && (password = ?)';
+    dungeonsDB.query(sql, params, (error, result) => {
+        if (!error) {
+            let respuesta = { ok: false, message: `Usuario o contraseña incorrectos` };                
+            if (result.length > 0) {
+                const { idUser, name, email, urlAvatar } = result[0]
+                let user = {  idUser, name, email, urlAvatar };
+                respuesta = { ok: true, message: `Login correcto`, resultado: user };
+            }
+            return res.status(200).json(respuesta);
+        } else {
+            let respuesta = { ok: false, message: error.sqlMessage };
+            return res.status(400).json(respuesta);
+        }
+    })
 };
 
 // Exportar controladores
