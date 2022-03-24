3757d968fe550500c2bc80b819dbf12b120595bc
PascualVera <pascualvera.994@gmail.com>
2022-03-24 19:09:10 +0100

    filterCharacter works!

--- controllers/character.js
+++ controllers/character.js
@@ -11,21 +11,4 @@ function getCharacter(req, res) {
 		}
 	})
 }
-function filterCharacter(req,res) {
-	let id = req.query.id
-	let sql = `SELECT characterpre.name, characterpre.idCharacter FROM characterpre
-	JOIN player ON (player.idCharacter = characterpre.idCharacter)
-	JOIN campaign ON (player.idCampaign = campaign.idCampaign)
-	WHERE campaign.idCampaign = '${id}'`
-	dungeonsDB.query(sql, (err,result)=>{
-		if(err){
-			let respuesta= {ok: false, message: err.sqlMessage}
-			res.status(400).json(respuesta)
-		}else{
-			let respuesta = {ok:true, respuesta:result}
-			res.status(200).json(respuesta)
-		}
-	})
-}
-
-module.exports = { getCharacter, filterCharacter }
+module.exports = { getCharacter }
