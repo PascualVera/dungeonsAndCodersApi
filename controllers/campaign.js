d62f0dd41239bd68ae9fd3e0d0960c8e38508745
ajurado301 <ajurado301@gmail.com>
2022-03-27 10:18:45 +0200

    salir, pausar, finalizar campaña, deleteCampaign (falta borrar los players)

--- controllers/campaign.js
+++ controllers/campaign.js
@@ -13,7 +13,7 @@ const getCampaign = (req, res) => {
     if (!idCampaign) {
         sql = "SELECT campaign.idCampaign, campaign.campaignName, campaign.numPlayer, campaign.maxPlayer, campaign.public, campaignpre.campaignName AS campaignNamePre, campaignpre.playerMin, campaignpre.playerMax FROM campaign JOIN campaignpre ON campaign.idCampaignPre = campaignpre.idCampaignPre WHERE campaign.numPlayer < campaign.maxPlayer AND NOT campaign.closed"
     } else {
-        sql = "SELECT campaign.idCampaign, campaign.campaignName, campaign.idCampaignPre, campaign.date, campaign.numPlayer, campaign.maxPlayer, campaign.public,campaign.closed, user.name FROM campaign JOIN user ON campaign.idMaster = user.idUser WHERE campaign.idCampaign = ?"
+        sql = "SELECT campaign.idCampaign, campaign.campaignName, campaign.idCampaignPre, campaign.date, campaign.numPlayer, campaign.maxPlayer, campaignpre.playerMin, campaign.public,campaign.closed, user.name FROM campaign JOIN user ON campaign.idMaster = user.idUser JOIN campaignpre ON campaign.idCampaignPre = campaignpre.idCampaignPre WHERE campaign.idCampaign = ?"
     };
     dungeonsDB.query(sql, params, (error, result) => {
         if (!error) {
@@ -100,24 +100,23 @@ const putCampaign = (req, res) => {
 
 // DELETE
 const deleteCampaign = (req, res) => {
-    return res.status(200).send({ ok: true, message: `deleteCampaign works!!` });
-
-    //  let params = [req.body.id_libro];
-    //  let sql = "DELETE FROM libro WHERE id_libro = ?";
-    // dungeonsDB.query(sql, params, (error, result) => {
-    //     if (!error) {
-    //         let respuesta;
-    //         if (result.affectedRows == 0){
-    //             respuesta = { ok: false, message: `Libro con id ${req.body.id_libro} no encontrado`};
-    //         }else {
-    //             respuesta = { ok: true, message: `Libro con id ${req.body.id_libro} eliminado`};
-    //         }
-    //         return res.status(200).json(respuesta);            
-    //     }else {
-    //         let respuesta = { ok: false, message: error.sqlMessage };
-    //         return res.status(200).json(respuesta);
-    //     }
-    // })
+    const { idCampaign } = req.body;
+    let params = [idCampaign];
+    let sql = "DELETE FROM campaign WHERE idCampaign = ?";
+    dungeonsDB.query(sql, params, (error, result) => {
+        if (!error) {
+            let respuesta;
+            if (result.affectedRows == 0){
+                respuesta = { ok: false, message: `Campaña con id ${idCampaign} no encontrada`};
+            }else {
+                respuesta = { ok: true, message: `Campaña con id ${idCampaign} eliminada`};
+            }
+            return res.status(200).send(respuesta);            
+        }else {
+            let respuesta = { ok: false, message: error.sqlMessage };
+            return res.status(400).send(respuesta);
+        }
+    })
 };
 
 // Exportar controladores
